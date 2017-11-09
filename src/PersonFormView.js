2564240108a0f7d272cc7032101bc5924ac317d6
Tjerk Valentijn <tjerk.valentijn@inology.nl>
2017-11-09 15:17:51 +0100

    formatting

--- src/PersonFormView.js
+++ src/PersonFormView.js
@@ -4,7 +4,9 @@ const PersonFormView = (person, onChangeHandler, onSaveHandler) => (
   <div>
     <div className="card horizontal">
       <div className="card-image">
-        <img src="img/user-male-silhouette.jpg" alt="male" style={{width: "100px", margin: "1em"}} />
+        <img src="img/user-male-silhouette.jpg"
+             alt="male"
+             style={{width: "100px", margin: "1em"}} />
       </div>
       <div className="card-stacked">
         <div className="card-content">
